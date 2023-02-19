cabfbd4b949716ecb0fef30680bf34d707ad46bd
Tanel Tekko <taneldeviis@hotmail.com>
2023-02-19 13:09:42 +0200

    tickets crud endpoints

--- Server/controllers/ticketController.js
+++ Server/controllers/ticketController.js
@@ -1,13 +1,78 @@
-import asyncHandler from 'express-async-handler'
-import { Ticket } from '../models/ticketModel.js'
-import { User } from '../models/userModel.js'
+import asyncHandler from "express-async-handler";
+import { Ticket } from "../models/ticketModel.js";
+import { User } from "../models/userModel.js";
 
 const getTickets = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'get tickets'})
-})
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not identified");
+  }
+  const tickets = await Ticket.find({ user: req.user.id });
+  res.status(200).json(tickets);
+});
+
+const getTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not identified");
+  }
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+  res.status(200).json(ticket);
+});
+
+const deleteTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not identified");
+  }
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+  await ticket.remove();
+  res.status(200).json({ success: true });
+});
+const updateTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not identified");
+  }
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+  const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body);
+  res.status(200).json(updatedTicket);
+});
 
 const createTicket = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'get tickets'})
-})
+  const { description, project } = req.body;
+  if (!project || !description) {
+    res.status(400);
+    throw new Error("Please add required fields");
+  }
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not identified");
+  }
+  const ticket = await Ticket.create({
+    project,
+    description,
+    user: req.user.id,
+    status: "New",
+  });
+  res.status(201).json(ticket);
+});
 
-export {getTickets, createTicket}
+export { getTickets, createTicket, getTicket, deleteTicket, updateTicket };
--- Server/routes/ticketRoutes.js
+++ Server/routes/ticketRoutes.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { createTicket, getTickets } from "../controllers/ticketController.js";
+import {
+  createTicket,
+  deleteTicket,
+  getTicket,
+  getTickets,
+  updateTicket,
+} from "../controllers/ticketController.js";
 import { authCheck } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(authCheck, getTickets).post(authCheck, createTicket);
+router
+  .route("/:id")
+  .get(authCheck, getTicket)
+  .put(authCheck, updateTicket)
+  .delete(authCheck, deleteTicket);
 
 export default router;
